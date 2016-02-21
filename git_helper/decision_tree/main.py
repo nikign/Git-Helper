e776e1540095181de9c8e392d7fbfaac3c1c5edc
Yihuan Dong <ydong2@ncsu.edu>
2016-02-21 16:23:41 -0500

    Added color to terminal output

---
+++ git_helper/decision_tree/main.py
@@ -0,0 +1,7 @@
+from colorama import init
+init()
+
+from colorama import Fore, Back, Style
+print(Fore.RED + Back.GREEN + 'Welcome to git helper!')
+print(Style.RESET_ALL)
+print('back to normal now')
