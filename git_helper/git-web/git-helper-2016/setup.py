11d9d222e4bcb3195bc6c7575065096bd11be4d7
boyzhirui <zhirui1992@gmail.com>
2016-02-26 12:51:32 -0500

    install necessary packages
    All the required packages are mentioned in lib_requirements.txt, run
    setup.py to install those packages.

---
+++ git_helper/git-web/git-helper-2016/setup.py
@@ -0,0 +1,8 @@
+#used for install necessary python modules to run the Google App Engine
+#After install, need to copy all the packages into libs folder
+import subprocess
+
+bashCommand = "sudo pip install -r lib_requirements.txt"
+process = subprocess.Popen(bashCommand.split(), stdout=subprocess.PIPE)
+output = process.communicate()[0]
+print output
