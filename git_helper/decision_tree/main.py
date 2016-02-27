beeeb467733dccb63686626aba33821fc5ac63e8
Yihuan Dong <ydong2@ncsu.edu>
2016-02-27 15:52:31 -0500

    Finish adding solution for push command

--- git_helper/decision_tree/main.py
+++ git_helper/decision_tree/main.py
@@ -127,19 +127,31 @@ def providePushSolution(msg):
     explanation = ''
     solution = ''    
     command = ''
-
-    #raise NotImplementedError('solution for push command has not been implemented yet')
-    if msg.find('[rejected]') > 0 and msg.find('failed to push some refs to') > 0 and (msg.find('(fetch first)') > 0 or msg.find('(non-fast-forward)')):
-        explanation = 'The remote server has some work that you do not have on your local machine. You can do a git pull command to get the work you do not have locally.'
-        command = ['git pull']
-        solution = ["Please use git pull command to get the work that you don't have locally."]
-    elif msg.find('src refspect') > 0 and msg.find('does not match any') > 0:
-        explanation = '' 
-    
-    else:
-        explanation = constant.noSolutionMessage
-        solution = constant.noSolutionSolution
-    printSolution(explanation,command,solution)
+    try:
+        #raise NotImplementedError('solution for push command has not been implemented yet')
+        if msg.find('[rejected]') > 0 and msg.find('failed to push some refs to') > 0 and (msg.find('(fetch first)') > 0 or msg.find('(non-fast-forward)')):
+            explanation = 'The remote server has some work that you do not have on your local machine. You can do a git pull command to get the work you do not have locally.'
+            command = ['git pull']
+            solution = ["Please use git pull command to get the work that you don't have locally."]
+        elif msg.find('src refspec') > 0 and msg.find('does not match any') > 0:
+            begin = msg.find('src refspec') + len('src refspec')
+            end = msg.find('does not match any')
+            branchName = msg[begin:end].lstrip().rstrip()
+            explanation = 'You get this message most likely because git will not let you to push a completely empty repository to ' + branchName + ' branch. You will at least need to have one file inside your local repository (folder), then add, commit and push.' 
+            command = ['git add <file-name>', 
+                        'git commit -m "Your commit message here"', 
+                        'git push origin ' + branchName]
+            solution = ['Make sure you have at least one file in your repository folder.',
+                        'Use ' + Style.DIM + command[0] + ' to add files into this commit. Use "." or "-A" for <file-name> if you want to add all files in the repository to this commit.',
+                        'Use ' + Style.DIM + command[1] + ' to commit to local repository.',
+                        'Use ' + Style.DIM + command[2] + ' to push you commit to ' + branchName + ' of remote repository.']
+        else:
+            explanation = constant.noSolutionMessage
+            solution = constant.noSolutionSolution
+        printSolution(explanation,command,solution)
+    except Exception as e:
+        print('Error in providePushSolution():')
+        print(e)
     return
 
 # Provide solution with decision tree ########################################################################################
