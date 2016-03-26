1d1634127cc458137b2a89453d99ad65d26aabd8
lintingting <lxue3@ncsu.edu>
2016-03-26 02:11:56 -0400

    update

--- git_helper/rate_results.py
+++ git_helper/rate_results.py
@@ -1,10 +1,11 @@
 from GoogleSearch.googlesearch import main_search_web
 from GoogleSearch.googlesearch import main_search_email
 
-# Query = "CONFLICT (content): Merge conflict in README.md.Automatic merge failed: fix conflicts and then commit the result."
+from GoogleSearch.DB_Search import main_searchDB_web
+from GoogleSearch.DB_Search import main_searchDB_email
 
 #Query = "CONFLICT (content): Merge conflict in README.md.Automatic merge failed: fix conflicts and then commit the result."
-path = "/Users/Linting/Google Drive/CSC510/Project/SE-16/"
+path = "/Users/Linting/Google Drive/CSC510/Project/Git-Helper/git_helper/GoogleSearch"
 
 
 def get_web_results(search_phrase):
@@ -13,7 +14,8 @@ def get_web_results(search_phrase):
     return result
 
 def get_email_result(error_message):
-    res = main_search_email(error_message)
+    res = main_searchDB_email(error_message,path)
+    #res = main_search_email(error_message)
     return res
 
 #print get_email_result(Query)
